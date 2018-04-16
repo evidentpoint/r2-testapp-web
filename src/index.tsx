a7b4e04d7175645121bd3f219137d58017abaeb3
Juan Corona <juan.carlos.corona.r@gmail.com>
2018-04-16 14:19:07 -0700

    Add polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
+// tslint:disable-next-line
+import 'es6-shim';
+// tslint:disable-next-line
+import 'promise-polyfill/src/polyfill';
+// tslint:disable-next-line
+import 'whatwg-fetch';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
