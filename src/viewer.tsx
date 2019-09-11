657fbbace976853119c79cac12a32f9be1ea072f
Michael Liu <mikeliu@evidentpoint.com>
2019-09-10 17:24:05 -0700

    Implemented abstracted Font size changer and font size query (though it's implemented as a state in the Viewer)

--- src/viewer.tsx
+++ src/viewer.tsx
@@ -1,22 +1,41 @@
 import React, { CSSProperties, ReactNode } from 'react';
 
-import { ReadiumNGNavControl } from './ng-nav-control';
 import { ReadiumNGView } from './ng-view';
 import { ReadiumNGViewSetting } from './ng-view-setting';
 
-import { Navigator, Rendition, RenditionContext } from '@readium/navigator-web';
+import { Navigator, Rendition, RenditionContext, SettingName } from '@readium/navigator-web';
 
 export interface IReadiumNGViewerStates {
   rendition: Rendition | null;
   navigator: Navigator | null;
+  fontSize: number;
 }
 
 export class ReadiumNGViewer extends React.Component<{}, IReadiumNGViewerStates> {
 
   constructor(props: {}) {
     super(props);
-    this.state = { rendition: null, navigator: null };
+
+    this.state = {
+      rendition: null,
+      navigator: null,
+
+      // Start with a font size of 100(%?)
+      fontSize: 100,
+    };
     this.renditionUpdated = this.renditionUpdated.bind(this);
+
+    // @ts-ignore
+    if (!window.ReadiumSDK) {
+      // @ts-ignore
+      window.ReadiumSDK = {};
+    }
+
+    // Instead of rendering a navigation component, register as interface to be called from outside
+    this.registerNavigation();
+
+    // Instead of View Settings, register Settings interface to be called from outside
+    this.registerViewSettingsInterface();
   }
 
   public renditionUpdated(rendCtx: RenditionContext): void {
@@ -42,16 +61,79 @@ export class ReadiumNGViewer extends React.Component<{}, IReadiumNGViewerStates>
           enableScroll={ false } viewAsVertical={ false }
           onRenditionCreated={ this.renditionUpdated }
           style={ { gridArea: 'BookContent' } }/>
-        <ReadiumNGNavControl
-            navigator={ this.state.navigator }
-            style={ { gridArea: 'NavControl' } }
-        />
+
+        {/*
         <ReadiumNGViewSetting
           rendition={ this.state.rendition }
           navigator={ this.state.navigator}
           style={ { gridArea: 'ViewSetting' } }/>
+          */}
       </div>
     );
+  }
+
+  public registerNavigation(): void {
+
+    // @ts-ignore
+    ReadiumSDK.nextScreen = async () : Promise<void> => {
+      if (this.state.navigator) {
+        await this.state.navigator.nextScreen();
+      }
+    };
+
+    // @ts-ignore
+    ReadiumSDK.prevScreen = async () : Promise<void> => {
+      if (this.state.navigator) {
+        await this.state.navigator.previousScreen();
+      }
+    };
+
+  }
+
+  public registerViewSettingsInterface(): void {
+
+    // @ts-ignore
+    ReadiumSDK.getCurrentFontSize = () : int => {
+      // Currently implemented as a state in the Viewer
+      // Should figure out if Navigator should have an API for this
+      return this.state.fontSize;
+    };
+
+    // @ts-ignore
+    ReadiumSDK.setFontSize = async (size : string) : void => {
+
+      let loc;
+      if (this.state.navigator) {
+        loc = await this.state.navigator.getCurrentLocationAsync();
+      }
+
+      if (this.state.rendition !== null) {
+        const newSetting = {
+          name: SettingName.FontSize,
+          value: size,
+        };
+        this.state.rendition.updateViewSettings([newSetting]);
+      }
+
+      if (loc && this.state.navigator) {
+        await this.state.navigator.gotoLocation(loc);
+      }
+
+      // Will simply have to keep track of the Font Size in state
+      this.setState({ fontSize: parseInt(size, 10) });
+
+    };
+
+  }
+
+  public componentDidMount(): void {
+    // @ts-ignore
+    Android.showButtons(true);
+  }
+
+  public componentWillUnmount(): void {
+    // @ts-ignore
+    Android.showButtons(false);
   }
 
 }
